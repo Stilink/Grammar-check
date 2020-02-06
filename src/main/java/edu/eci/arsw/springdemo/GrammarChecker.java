80b871cca9cf4490abd29d58a4f0c90fe83c1be6
Stilink <jeymar.vega@labinfo.is.escuelaing.edu.co>
2020-02-06 08:02:30 -0500

    Primera parte

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -1,11 +1,13 @@
 package edu.eci.arsw.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class GrammarChecker {
-
+	@Autowired
+	@Qualifier("spanishSpellChecker")
 	SpellChecker sc;
 
 	String x;
@@ -23,7 +25,7 @@ public class GrammarChecker {
 	public String check(String text){
 		
 		StringBuffer sb=new StringBuffer();
-		sb.append("Spell checking output:"+sc.checkSpell(text));
+		sb.append("Spell checking output:"+sc.checkSpell(text)+"\n");
 		sb.append("Plagiarism checking output: Not available yet");
 		
 		
--- src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -3,6 +3,9 @@ package edu.eci.arsw.springdemo;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpanishSpellChecker implements SpellChecker {
 
 	@Override
